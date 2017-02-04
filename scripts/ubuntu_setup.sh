a3395cc235530eea3b2ef90b5d5c290fc3068e8e
Ray Chen <raychen0411@gmail.com>
2017-02-04 17:58:52 -0500

    add template setup scirpt for empty ubuntu server

---
+++ scripts/ubuntu_setup.sh
@@ -0,0 +1,8 @@
+# This is suitable for ubuntu
+# install fish
+sudo apt-get install fish
+chsh -s /usr/bin/fish
+
+# install vim, need to download vim_config.sh first
+sudo apt-get install vim
+sh vim_config.sh
