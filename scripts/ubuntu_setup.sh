61c84a82ec13de6d96417c7325d472e72ee91579
Ray Chen <raychen0411@gmail.com>
2017-02-14 19:30:47 -0500

    add more scripts and books

--- scripts/ubuntu_setup.sh
+++ scripts/ubuntu_setup.sh
@@ -1,7 +1,7 @@
 # This is suitable for ubuntu
 # install fish
 sudo apt-get install fish
-chsh -s /usr/bin/fish
+chsh -s 'which fish' 
 
 # install vim, need to download vim_config.sh first
 sudo apt-get install vim
